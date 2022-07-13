1adbd9def0f3df05b961a685fad58b5c5d1484ff
yatharthdwivedi <yatharthdwivedi1@gmail.com>
2022-07-13 19:11:26 +0530

    Final deployment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
 require("dotenv").config();
+const path = require("path")
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -24,6 +24,18 @@ app.use(cors());
 
 app.use("/auth", require("./routes/auth"));
 app.use("/notes", require("./routes/notes"));
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+} else {
+  app.get("/", (req,res)=> {
+    res.send("Api Running")
+  })
+}
 
 app.listen(process.env.PORT ||"5000", () => {
   connect()
